4031e2a98a3544b74fff7446753948b00768e925
meenakchi <meenakchis1@gmail.com>
2026-03-01 19:14:48 +0800

    commit

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function openWindow(id) {
 function closeWindow(id) {
   document.getElementById(id).style.display = 'none'
 }
+
+// Ensure bio window is open on page load
+window.addEventListener('load', function() {
+  openWindow('bio-window');
+});
